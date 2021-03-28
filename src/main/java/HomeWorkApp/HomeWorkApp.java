ade6e9e138c81688d51863974a32b6aa894b7903
pats <bixami4338@0335g.com>
2021-03-28 19:31:34 +0300

    ДЗ 2

--- src/main/java/HomeWorkApp/HomeWorkApp.java
+++ src/main/java/HomeWorkApp/HomeWorkApp.java
@@ -2,33 +2,58 @@ package HomeWorkApp;
 
 public class HomeWorkApp {
 
-    public static void printThreeWords() {
-        System.out.println("_Orange \n" + "_Banana \n" + "_Apple \n");
+    //Задание 1
+    public static boolean sum10to20(int a, int b) {
+        if ((a+b>=10)&&(a+b<=20)) return true;
+        else  return false;
     }
 
-    public static void checkSumSign() {
-        int a= (int)Math.random();
-        int b= (int)Math.random();
-        if (a+b>=0) System.out.println("Сумма положительная \n");
-        else  System.out.println("Сумма отрицательная \n");
+    //Задание 2
+    public static void positiveValue(int a) {
+       if (a>=0) System.out.println("\npositive value\n");
+       else System.out.println("\nnegative value\n");
     }
 
-    public static void printColor() {
-        int value= (int)Math.random();
-        if (value<=0) System.out.println("Красный \n");
-        if ((value>0) && (value<=100)) System.out.println("Желтый \n");
-        if (value>100) System.out.println("Зеленый \n");
+    //Задание 3
+    public static boolean negativeValue(int a) {
+        if (a>=0) return false;
+        else return true;
     }
-    public static void compareNumbers() {
-        int a= (int)Math.random();
-        int b= (int)Math.random();
-        if (a>=b) System.out.println("a>=b");
-        else System.out.println("a<b");
+
+    //Задание 4
+    public static void nString(String textLine, int n) {
+        for (int i=0; i<n; i++) System.out.println(i+1 + " " +textLine);
     }
+
+    //Задание 5
+    public static boolean leapYear(int year) {
+        if ((year%400==0)||((year%4==0)&&(year%100!=0))) return true;
+        else return false;
+    }
+
     public static void main (String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        int a= (int)Math.random();
+        int b= (int)Math.random();
+
+        //Задание 1
+        //System.out.println(sum10to20(a,b));
+        sum10to20(a,b);
+
+        //Задание 2
+        positiveValue(a);
+
+        //Задание 3
+        //System.out.println(negativeValue(a));
+        negativeValue(a);
+
+        //Задание 4
+        int n=4;
+        String textLine = "ABCD";
+        nString(textLine,n);
+
+        //Задание 5
+        System.out.println("\n"+leapYear(2000));
+        System.out.println(leapYear(2001));
+        System.out.println(leapYear(1900));
     }
 }
