22fa6082e6e09d2c529f9a8f14c414925c27f32a
pats <bixami4338@0335g.com>
2021-04-19 23:03:41 +0300

    ДЗ 8

--- src/main/java/HomeWorkApp/HomeWorkApp.java
+++ src/main/java/HomeWorkApp/HomeWorkApp.java
@@ -5,27 +5,6 @@ public class HomeWorkApp {
 
 
     public static void main(String[] args) {
-
-        Cat[] cats = {
-                new Cat("Мурзик",5,false),
-                new Cat("Бурзик",10,false),
-                new Cat("Зурзик",10,true),
-                new Cat("Шурзик",10,false)
-        };
-
-        Plate plate = new Plate(15);
-        plate.info();
-
-        for (Cat cat: cats) {
-            cat.eat(plate);
-        }
-
-        plate.increaseFood(25);
-        System.out.println("в тарелку добавлена еда");
-        plate.info();
-
-        for (Cat cat: cats) {
-            cat.eat(plate);
-        }
+        new MyWindow();
     }
 }
